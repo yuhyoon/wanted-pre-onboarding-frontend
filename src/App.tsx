5ed0bd4a29f2df8f0bb2590f77a40190b67bf885
ShineTiger <shinetiger.dev@gmail.com>
2023-01-29 15:17:49 +0900

    :hammer: 로그인, 투두리스트 라우터 설정 및 경로이동 기능 추가
    회원가입 페이지에서 정상적으로 가입이 완료되면  로그인 페이지(signin)로 이동, 로그인 페이지에서 정상적으로 로그인이 완료되면 투두리스트 페이지(todo)경로로 이동한다

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import SignUp from "./page/SignUp";
+import SignIn from "./page/SignIn";
+import Todos from "./page/Todos";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/todos" element={<Todos />}></Route>
       </Routes>
     </div>
   );
---
+++ src/page/SignIn.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import InputUserProfile from "../components/InputUserProfile";
+
+const SignIn = () => {
+  return (
+    <>
+      <h2>로그인</h2>
+      <InputUserProfile location="todo" attr="signin" />;
+    </>
+  );
+};
+
+export default SignIn;
